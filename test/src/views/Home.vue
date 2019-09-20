77ad418594a4f47adf91b3262e812408e6246135
TatagW <tatagwibowo95@gmail.com>
2019-09-20 10:49:01 +0700

    half way

--- test/src/views/Home.vue
+++ test/src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
 <div class="background">
+    <audio autoplay id="ost" loop hidden>
+      <source src="../assets/Pacman-sound.mp3" type="audio/mp3">
+      your audio doesnt work
+    </audio>
+    <!-- <iframe src="../assets/Pacman-sound.mp3" allow="autoplay" style="display:none" id="iframeAudio">
+    </iframe>  -->
     <div class="box" :style="{top: topPosition + 'vh', left: leftPosition + 'vw'}">
       <img v-if="direction === 'right'" class="box" src="../assets/right.png" alt="">
       <img v-else-if="direction === 'left'" class="box" src="../assets/left.png" alt="">
@@ -12,6 +18,7 @@
 </template>
 
 <script>
+import {Howl, Howler} from 'howler';
 export default {
     data: function(){
         return {
@@ -20,10 +27,15 @@ export default {
           direction: 'right',
           moving: null,
           foods: [],
-          score: 0
+          score: 0,
+          
+
         }
     },
     methods: {
+      turnOnSound(){
+
+      },
       handler(e){
         clearInterval(this.moving)
         this.moving = setInterval(() => {
@@ -54,11 +66,13 @@ export default {
       }
     },
     watch: {
+      
       leftPosition(){
           for(let el in this.foods){ 
-            if(this.topPosition < (this.foods[el][0] + 5) && this.topPosition > this.foods[el][0] && 
-              this.leftPosition < (this.foods[el][1] + 5) && this.leftPosition > this.foods[el][1]){
-              console.log('kena atas')
+            if(this.topPosition < (this.foods[el][0] + 7) && this.topPosition > (this.foods[el][0] - 2 )&& 
+              this.leftPosition < (this.foods[el][1] + 7) && this.leftPosition > (this.foods[el][1] -2)){
+              var audio = new Audio(require("../assets/pacman_chomp.wav"));
+              audio.play();
               this.score++
               this.foods.splice(el,1)
               if(this.foods.length=== 0)clearInterval(this.moving)
@@ -74,9 +88,10 @@ export default {
       topPosition(){
 
           for(let el in this.foods){ 
-            if(this.topPosition < (this.foods[el][0] + 5) && this.topPosition > this.foods[el][0] && 
-              this.leftPosition < (this.foods[el][1] + 5) && this.leftPosition > this.foods[el][1]){
-              console.log('kena atas')
+            if(this.topPosition < (this.foods[el][0] + 7) && this.topPosition > (this.foods[el][0] - 2)&& 
+              this.leftPosition < (this.foods[el][1] + 7) && this.leftPosition > (this.foods[el][1] - 2)){
+              var audio = new Audio(require("../assets/pacman_chomp.wav"));
+              audio.play();
               this.score++
               this.foods.splice(el,1)
               if(this.foods.length=== 0)clearInterval(this.moving)
@@ -90,13 +105,19 @@ export default {
         }
       }
     },
+    mounted(){
+      document.getElementById("ost").volume = 0.5
+    },
     created(){
+
+      var x = document.createElement("AUDIO")
+      x.setAttribute("src","../assets/Pacman-sound.mp3")
+      x.loo
       let totalFoods = Math.ceil(Math.random()*5)+4
       for(let i = 0; i < totalFoods; i++){
         this.foods.push([Math.ceil(Math.random()*90), Math.ceil(Math.random()*90)])
       }
       window.addEventListener('keyup', this.handler);
-   
     }
 }
 </script>
