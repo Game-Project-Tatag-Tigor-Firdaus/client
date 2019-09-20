1b773ca95ce282d3196c6be714e77be8e3bd67f3
das <ardynn.d@gmail.com>
2019-09-20 11:06:02 +0700

    aaaa

--- client-game/src/views/Home.vue
+++ client-game/src/views/Home.vue
@@ -1,13 +1,102 @@
 <template>
-  <HelloWorld />
+  <div>
+    <appBar @logoutSignal="logout" />
+    <v-container>
+      <v-row align="center" justify="start" no-gutters>
+        <v-col cols="10" class="pa-12">
+          <h2>Create New Room</h2>
+          <v-divider></v-divider>
+        </v-col>
+        <v-col cols="2" class="pa-12">
+          <div class="my-2">
+            <v-btn class="mx-2" fab dark color="orange" @click="triggerCreateRoom">
+              <v-icon dark>mdi-plus</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col cols="12">
+          <roomSelf />
+          <roomSelf />
+        </v-col>
+        <v-col cols="12" class="pa-12">
+          <h2>Available Rooms</h2>
+          <v-divider></v-divider>
+        </v-col>
+        <v-col cols="12">
+          <roomOthers />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <login :loginModal="loginModal" @statusLogin="changeLoginStatus" />
+    <createRoom :createModal="createModal" @createRoom="createRoom" @close="closeCreateModal" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+import login from "../components/Login";
+import roomSelf from "../components/RoomSelf";
+import roomOthers from "../components/RoomOthers";
+import appBar from "../components/AppBar";
+import createRoom from "../components/CreateRoom";
 
 export default {
   components: {
-    HelloWorld,
+    appBar,
+    login,
+    roomSelf,
+    roomOthers,
+    createRoom
   },
+  props: {
+    source: String
+  },
+  data: () => ({
+    username: localStorage.getItem("username"),
+    isLogin: false,
+    drawer: null,
+    drawerRight: null,
+    right: false,
+    left: false,
+    loginModal: false,
+    createModal: false
+  }),
+  methods: {
+    changeLoginStatus(cond, payload) {
+      console.log("============ Masuk ============");
+      localStorage.setItem("username", payload);
+      this.isLogin = cond;
+      this.checkLoginStatus();
+    },
+    checkLoginStatus() {
+      console.log(localStorage.getItem("username"));
+      if (localStorage.getItem("username")) {
+        this.isLogin = true;
+        this.triggerModal(false);
+      } else {
+        this.isLogin = false;
+        this.triggerModal(true);
+      }
+    },
+    triggerModal(bool) {
+      this.loginModal = bool;
+    },
+    logout() {
+      localStorage.removeItem("username");
+      this.checkLoginStatus();
+    },
+    createRoom(roomName) {
+      this.createModal = false;
+    },
+    triggerCreateRoom() {
+      this.createModal = true;
+    },
+    closeCreateModal() {
+      this.createModal = false;
+    }
+  },
+  created() {
+    this.checkLoginStatus();
+  }
 };
 </script>
